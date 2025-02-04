7ea3fec7ba1a84e71eb3bc08545b9e38e8ae79e4
Atul Prakash <berealatul@gmail.com>
2025-02-04 22:54:06 +0530

    Print HelloWorld PID & PPID

--- a01a.c
+++ a01a.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main()
 {
     printf("Hello, World\n");
+
+    int currentPID = getpid();
+    int currentPPID = getppid();
+
+    printf("currentPID: %d\n", currentPID);
+    printf("currentPPID: %d\n", currentPPID);
+
     return 0;
 }
