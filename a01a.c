00d9117b719dd3d0f01c602d9b2223b643a57687
Atul Prakash <berealatul@gmail.com>
2025-02-04 23:04:31 +0530

    Created a child process and printed its PID & PPID

--- a01a.c
+++ a01a.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main()
 {
@@ -10,6 +11,39 @@ int main()
 
     printf("currentPID: %d\n", currentPID);
     printf("currentPPID: %d\n", currentPPID);
+
+    int child = fork();
+    /*
+        fork returns three kinds of value
+        > 0     : returning to parent process
+        == 0    : creating child process
+        < 0     : failed to create fork
+    */
+
+    if (child < 0)
+    {
+        printf("Fork Failed\n");
+        exit(0);
+    }
+    else if (child == 0)
+    {
+        // new child
+        int childPID = getpid();
+        int childPPID = getppid();
+
+        printf("childPID: %d\n", childPID);
+        printf("childPPID: %d\n", childPPID);
+    }
+    else
+    {
+        printf("Returning To Parent After Child Processesed!\n");
+
+        int parentPID = getpid();
+        int parentPPID = getppid();
+
+        printf("parentPID: %d\n", parentPID);
+        printf("parentPPID: %d\n", parentPPID);
+    }
 
     return 0;
 }
