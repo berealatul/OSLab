26ad20e04d2379bdecd1b909c4218373e7da00e5
Atul Prakash <berealatul@gmail.com>
2025-02-06 00:15:08 +0530

    Created a function prototype to handle interrupt signal

--- a02a.c
+++ a02a.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+
+void handleSignal(int sig)
+{
+    printf("\nInterrut Signal Recieved\n");
+    // TODO
+    exit(0);
+}
 
 int main()
 {
     int counter = 0;
     printf("Time Elapsed: ");
+
+    // register signal handler for SIGINT (Ctrl + C)
+    signal(SIGINT, handleSignal);
 
     while (1)
     {
