b2c2ff599b0a23f2b3bd977d650a0173ab5f81a8
Atul Prakash <berealatul@gmail.com>
2025-02-05 21:53:54 +0530

    a program to print argument vector content

--- a01c/childProcess.c
+++ a01c/childProcess.c
@@ -2,12 +2,13 @@
 
 int main(int argc, char *argv[])
 {
-    printf("Child Process Executed: %s\n", argv[0]);
+    printf("Child Process Started:\n");
+
     for (int i = 0; i < argc; i++)
     {
         printf("Argument %d: %s\n", i, argv[i]);
     }
 
-    printf("Child Program Finished\n");
+    printf("Child Process Completed\n");
     return 0;
 }
