a8839925baadb5869aae37d65fef5502c7fd6292
Atul Prakash <berealatul@gmail.com>
2025-02-06 00:28:49 +0530

    Handled interrupt by creating fork and printing PID & PPID from child and parent

--- a02a.c
+++ a02a.c
@@ -2,11 +2,33 @@
 #include <unistd.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 void handleSignal(int sig)
 {
     printf("\nInterrut Signal Recieved\n");
-    // TODO
+    pid_t child = fork();
+
+    if (child < 0)
+    {
+        printf("Fork Failed\n");
+        exit(1);
+    }
+    else if (child == 0)
+    {
+        printf("Child Process: \n");
+        printf("    childPID: %d\n", getpid());
+        printf("    childPPID: %d\n", getppid());
+    }
+    else
+    {
+        wait(NULL);
+        printf("Parent Process: \n");
+        printf("    parentPID: %d\n", getpid());
+        printf("    childPID: %d\n", child);
+    }
+
     exit(0);
 }
 
