240dd3b29649de52885f07ae8ef593a71ad17d46
Atul Prakash <berealatul@gmail.com>
2025-02-06 19:38:08 +0530

    Created a thread function to greet

--- a02c.c
+++ a02c.c
@@ -5,8 +5,20 @@ Use pthread_exit() in your program (if possible) for terminating the thread.
 */
 
 #include <stdio.h>
+#include <pthread.h>
+
+// since c does not have facility to accept generic data type so casting using (void*) to resolve it
+void *threadFunction()
+{
+    printf("Hello World\n");
+}
 
 int main()
 {
+    printf("Running thread...\n");
+    pthread_t thread;
+    pthread_create(&thread, NULL, threadFunction, NULL);
+    printf("Parent Completed\n");
+
     return 0;
 }
