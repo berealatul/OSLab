a9f50f244194a1e9b63d9a6db90ad48a8f50283e
Atul Prakash <berealatul@gmail.com>
2025-02-06 19:12:17 +0530

    Starting Assignment 2(C)

---
+++ a02c.c
@@ -0,0 +1,12 @@
+/*
+Write a C program to create a user level thread using system call pthread_create( )
+and assign the thread to display the “HELLO WORLD” .
+Use pthread_exit() in your program (if possible) for terminating the thread.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    return 0;
+}
